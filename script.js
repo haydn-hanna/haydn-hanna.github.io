c8c94b04b31f02775e3f29c58ded89aa5a251baf
Haydn Hanna <haydn.hanna@gmail.com>
2021-10-05 12:24:58 -0400

    removed calculated viewport units

--- script.js
+++ script.js
@@ -38,20 +38,11 @@ function normalizeSlideHeights() {
     
 }
 
-function defineViewportHeight(){
-    let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
 
-
-$(window).on('load resize', ()=>{
+$(window).on('load resize orientationchange', ()=>{
     normalizeSlideHeights();
-    defineViewportHeight();
 });
 
-$(window).on('orientationchange',()=>{
-    normalizeSlideHeights();
-})
+
 
 
