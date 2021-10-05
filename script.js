b33fd4377fd66eba364f2553637cfc96f3243f8f
Haydn Hanna <haydn.hanna@gmail.com>
2021-10-05 11:59:11 -0400

    add --vh as a calculated css property

--- script.js
+++ script.js
@@ -38,6 +38,20 @@ function normalizeSlideHeights() {
     
 }
 
+function defineViewportHeight(){
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+
+function ensureProperFit(){
+    normalizeSlideHeights();
+    defineViewportHeight();
+}
+
 $(window).on(
     'load resize orientationchange', 
-    normalizeSlideHeights);
+    ensureProperFit);
+
+
